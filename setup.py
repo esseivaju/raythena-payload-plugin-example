71733695828eae4d2b60e3345425f1f9d8146faa
Julien Esseiva <esseivaju@gmail.com>
2020-01-08 14:12:27 -0800

    Updated package name

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='Raythena-plugins-example',
+    name='raythena-plugins-example',
     version='1.0',
     author='Julien Esseiva',
     author_email='julien.esseiva@hefr.ch',
-    packages=find_namespace_packages(include=['Raythena.*'])
+    packages=find_namespace_packages(include=['raythena.*'])
 )
